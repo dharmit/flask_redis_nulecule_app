814282bbd21dd593935ec3bf26b6c8519022abbd
Dharmit Shah <dshah@redhat.com>
2015-10-27 12:04:04 +0530

    Flask and Kubernetes
    - Modified flask code to generalize it for Docker and Kubernetes use.
    - Added Kubernetes artifects

--- flask/app/app/views.py
+++ flask/app/app/views.py
@@ -1,13 +1,20 @@
 from app import app
+from urlparse import urlparse
+import os
 import redis
 
-r = redis.StrictRedis(host="redis", port="6379", db=0)
+redis_port = os.environ['REDIS_PORT']
+
+r = redis.StrictRedis(host=urlparse(redis_port).hostname,
+                      port=urlparse(redis_port).port,
+                      db=0)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     if r.get("count") == None:
-	r.set("count", 0)
+        r.set("count", 0)
     c = int(r.get("count"))
     c += 1
     r.set("count", c)
